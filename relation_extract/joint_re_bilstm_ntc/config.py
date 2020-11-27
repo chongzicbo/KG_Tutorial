ea3da5a0d2b4d5989157e61cdc1b092a195f1b27
chongzicbo <361848641@qq.com>
2020-11-27 21:46:57 +0800

    添加模型保存路径

--- relation_extract/joint_re_bilstm_ntc/config.py
+++ relation_extract/joint_re_bilstm_ntc/config.py
@@ -6,11 +6,14 @@ TRAIN_PATH = root_dir + 'train.json'
 TEST_PATH = root_dir + 'test.json'
 WORD_DICT = root_dir + 'word_dict.txt'
 TAG_DICT = root_dir + 'tag_dict.txt'
+model_save_path = os.path.abspath(os.path.dirname(os.path.dirname(os.path.dirname(__file__)))) + "/models/joint_re_bilstm.h5"
 
 MAX_LEN = 200
 EMBEDDING_SIZE = 300
 DROPOUT = 0.5
 LSTM_ENCODE = 300
 LSTM_DECODE = 600
-EPOCH_NUM = 1000
+EPOCH_NUM = 100
 BATCH_SIZE = 16
+
+print(model_save_path)
